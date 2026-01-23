6831b165541300aff91f5cc4754a92131363a436
hemanthroshan <hemanthroshan@users.noreply.github.com>
2026-01-23 08:07:29 +0000

    deploy: e09ce1a51d39b1436d771e56a4260aaec77a68bd

---
+++ chunk-OCSYT5ON.js
@@ -0,0 +1,1 @@
+import{Aa as o,Qa as n,Ra as a,Va as i}from"./chunk-R3PNEIWV.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,c){t&1&&(n(0,"p"),i(1,"about works!"),a())},encapsulation:2})};export{s as About};
